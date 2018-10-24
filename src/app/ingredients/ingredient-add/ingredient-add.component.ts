d649e06edb60a42dc141ec301407110f5eb0a65c
Verhoevemans <verhoevemans@gmail.com>
2018-10-24 22:55:12 +0200

    update project, add instruction pdf

--- src/app/ingredients/ingredient-add/ingredient-add.component.ts
+++ src/app/ingredients/ingredient-add/ingredient-add.component.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { IngredientsService } from '../ingredients.service';
 import { Ingredient } from '../../models/ingredient.model';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 
 @Component({
   selector: 'app-ingredient-add',
@@ -13,15 +13,22 @@ export class IngredientAddComponent implements OnInit {
   ingredientAmount: number;
   ingredientIndex: number = null;
 
-  constructor(private ingredientsService: IngredientsService, private route: ActivatedRoute) { }
+  constructor(private ingredientsService: IngredientsService,
+              private route: ActivatedRoute,
+              private router: Router) {
+  }
 
   ngOnInit() {
     this.route.params.subscribe((params: Params) => {
       if (params['id']) {
         this.ingredientIndex = +params['id'];
         const ingredient = this.ingredientsService.getIngredient(this.ingredientIndex);
-        this.ingredientName = ingredient.name;
-        this.ingredientAmount = ingredient.amount;
+        if (ingredient) {
+          this.ingredientName = ingredient.name;
+          this.ingredientAmount = ingredient.amount;
+        } else {
+          this.router.navigate(['ingredients']);
+        }
       } else {
         this.ingredientIndex = null;
       }
@@ -29,10 +36,19 @@ export class IngredientAddComponent implements OnInit {
   }
 
   addIngredient(): void {
-    console.log('Dit zijn de ingredienten:', this.ingredientName, this.ingredientAmount);
     this.ingredientsService.addIngredient(new Ingredient(this.ingredientName, this.ingredientAmount));
     this.ingredientName = null;
     this.ingredientAmount = null;
+  }
+
+  deleteIngredient() {
+    this.ingredientsService.updateIngredient(this.ingredientIndex, null);
+    this.router.navigate(['ingredients']);
+  };
+
+  updateIngredient() {
+    this.ingredientsService.updateIngredient(this.ingredientIndex, new Ingredient(this.ingredientName, this.ingredientAmount));
+    this.router.navigate(['ingredients']);
   }
 
   isDisabled(): boolean {
--- src/app/ingredients/ingredients.service.ts
+++ src/app/ingredients/ingredients.service.ts
@@ -11,7 +11,7 @@ export class IngredientsService {
   constructor(private httpClient: HttpClient) {}
 
   getIngredient(index): Ingredient {
-    return this.ingredients.slice(index, index + 1)[0];
+    return this.ingredients ? this.ingredients.slice(index, index + 1)[0] : null;
   }
 
   getIngredients(): Ingredient[] {
@@ -20,7 +20,11 @@ export class IngredientsService {
     } else {
       this.httpClient.get<Ingredient[]>('https://ordina-fontys-workshop.firebaseio.com/ingredients.json')
         .subscribe((ingredients) => {
-          this.ingredients = ingredients;
+          if (ingredients) {
+            this.ingredients = ingredients;
+          } else {
+            this.ingredients = [];
+          }
           this.ingredientsChanged.next(this.ingredients.slice());
         });
 
@@ -36,6 +40,20 @@ export class IngredientsService {
       this.ingredients
     ).subscribe((ingredients: Ingredient[]) => {
       this.ingredientsChanged.next(ingredients);
-    })
+    });
+  }
+
+  updateIngredient(index, ingredient) {
+    if (ingredient) {
+      this.ingredients.splice(index, 1, ingredient);
+    } else {
+      this.ingredients.splice(index, 1);
+    }
+    this.httpClient.put(
+      'https://ordina-fontys-workshop.firebaseio.com/ingredients.json',
+      this.ingredients
+    ).subscribe((ingredients: Ingredient[]) => {
+      this.ingredientsChanged.next(ingredients);
+    });
   }
 }
